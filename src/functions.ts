bb497b10aff0070e323cc2f9af923ef877605050
Nima Birgani <nima.birgani@gmail.com>
2024-01-23 10:10:58 -0800

    first update

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,5 @@
 import { Store } from './types';
 
-export function showStore(s: Store): string {
-  return s.name;
+export function showStore(store: Store): string {
+  return `Your Store name is: ${store.name}`;
 }
