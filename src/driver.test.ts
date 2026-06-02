75de291ba648e8c8c947e4e6afffd50ad9f57502
Syed Ahmed <syed@deltastream.io>
2026-06-02 09:57:14 -0700

    update api endpoint which was failing ts test

--- src/driver.test.ts
+++ src/driver.test.ts
@@ -5,20 +5,20 @@ import { AuthenticationError } from './error.ts';
 describe('driver', () => {
   it('should throw auth error if no token provided', async () => {
     expect(() =>
-      createConnection('https://api.deltastream.io/v2?sessionID=123')
+      createConnection('https://api-kap822.deltastream.io/v2?sessionID=123')
     ).toThrowError(AuthenticationError);
   });
 
   it('should throw auth error if invalid token provided', async () => {
     let c = createConnection(
-      'https://_:xx@api.deltastream.io/v2?sessionID=123'
+      'https://_:xx@api-kap822.deltastream.io/v2?sessionID=123'
     );
     await expect(() => c.version()).rejects.toThrowError(AuthenticationError);
   });
 
   it('should succeed if valid token provided', async () => {
     let c = createConnection(
-      'https://_:sometoken@api.deltastream.io/v2?sessionID=123'
+      'https://_:sometoken@api-kap822.deltastream.io/v2?sessionID=123'
     );
     let version = await c.version();
     expect(version).toEqual({ major: 2, minor: 0, patch: 0 });
@@ -26,7 +26,7 @@ describe('driver', () => {
 
   it('should succeed if valid token provided', async () => {
     let c = createConnection(
-      'https://api.deltastream.io/v2?sessionID=123',
+      'https://api-kap822.deltastream.io/v2?sessionID=123',
       async () => 'sometoken'
     );
     let version = await c.version();
